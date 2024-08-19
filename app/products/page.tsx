e411659f9e03070ae425d1fed02e7a0050c06e55
= <=>
2024-08-19 23:28:16 +0500

    commit

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,16 +1,16 @@
 import ProductsContainer from "@/components/products/ProductsContainer";
 
 function ProductsPage({
-    searchParams,
+  searchParams,
 }: {
-    searchParams: { search?: string; layout?: string };
+  searchParams: { search?: string; layout?: string };
 }) {
-    const search = searchParams.search || "";
-    const layout = searchParams.layout || "grid";
-    return (
-        <>
-            <ProductsContainer search={search} layout={layout} />
-        </>
-    );
+  const search = searchParams.search || "";
+  const layout = searchParams.layout || "grid";
+  return (
+    <>
+      <ProductsContainer search={search} layout={layout} />
+    </>
+  );
 }
 export default ProductsPage;
