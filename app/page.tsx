6474eb27a3b0fb3122440052d040fc14253f85e7
= <=>
2024-08-21 15:06:00 +0500

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,13 @@ import FeaturedProducts from "@/components/home/FeaturedProducts";
 import Hero from "@/components/home/Hero";
 
 function HomePage() {
-    return (
-        <>
-            <Hero />
-            <Suspense fallback={<LoadingContainer />}>
-                <FeaturedProducts />
-            </Suspense>
-        </>
-    );
+  return (
+    <>
+      <Hero />
+      <Suspense fallback={<LoadingContainer />}>
+        <FeaturedProducts />
+      </Suspense>
+    </>
+  );
 }
 export default HomePage;
