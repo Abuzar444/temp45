7fa491eefee112cb01aefde36ea789b64a52d57c
= <=>
2024-08-21 15:16:12 +0500

    commit

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,18 +3,18 @@ import { ThemeProvider } from "./theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 
 function Providers({ children }: { children: React.ReactNode }) {
-    return (
-        <>
-            <Toaster />
-            <ThemeProvider
-                attribute='class'
-                defaultTheme='system'
-                enableSystem
-                disableTransitionOnChange
-            >
-                {children}
-            </ThemeProvider>
-        </>
-    );
+  return (
+    <>
+      <Toaster />
+      <ThemeProvider
+        attribute='class'
+        defaultTheme='system'
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </>
+  );
 }
 export default Providers;
